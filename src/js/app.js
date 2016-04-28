7198b3a20bf5c4d6bd42c5d5701c9a74b4cbd20e
calmajam <giampiero.calma@gmail.com>
2016-04-28 11:05:40 +0200

    Revert "prima lezione"
    This reverts commit 10cbc27982d35365d21c521fa536990b63ab950d.

--- src/js/app.js
+++ src/js/app.js
@@ -11,31 +11,3 @@ var CONFIG = {
     },
     IOS: (navigator.userAgent.match(/iPad|iPhone|iPod/g) ? true : false),
 };
-
-
-var app = angular.module('ullo', []); // module crea nuove applicazioni o nuovi plugin
-
-// l'array si usa per minimizzazione, in modo che l'ordine tra l'array e i parametri venga preservato e angular riconosca i parametri minimizzati
-// $scope = oggetto di angular che contiene gli oggetti del DOM agganciati al controller
-app.controller('testCtrl', ['$scope', '$timeout', '$http', function($scope, $timeout, $http) {
-    $scope.model = {
-        title: 'Titolo',
-        description: 'description'
-    }
-    
-    $timeout(function () {
-        $scope.model.title = 'Titolone'
-    }, 2000);
-    
-    $scope.onClick = function() {
-        $scope.model.description = 'description 2'
-    };
-    
-    // servizio per chiamate ajax
-    // then = promise che consente di gestire due callback, una in caso di successo, una in caso di errore
-    $http.get('http://ulloapi.wslabs.it/api/stream/anonymous').then(function (response) {
-        $scope.model.items = response.data;
-    }), function (error) {
-        console.log(error);
-    }
-}]);
